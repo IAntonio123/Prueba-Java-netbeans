1364127c51f34f745d0f25082345453a2828f0c6
Antonio M <antoniomayhuay@gmail.com>
2022-12-25 10:18:34 -0500

    agregando funcionalidad botones del form Cliente

--- src/Controlador/ControladorCliente.java
+++ src/Controlador/ControladorCliente.java
@@ -1,11 +1,17 @@
 package Controlador;
 
 import DAO.CRUD_Cliente;
+import Formato.Mensajes;
 import VIsta.*;
 import modelo.*;
 import Procesos.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ControladorCliente implements ActionListener{
     FormCliente vista;
@@ -15,19 +21,60 @@ public class ControladorCliente implements ActionListener{
         vista=fc;
         vista.btnRegistrar.addActionListener(this);
         vista.btnConsultar.addActionListener(this);
+        vista.btnActualizar.addActionListener(this);
         ProcesosCliente.Presentacion(fc);
         AcualizarForm();
     }
     void AcualizarForm(){
         crud=new CRUD_Cliente();
         crud.MostrarClientesEnTabla(vista.tblCliente);
+        ProcesosCliente.LimpiarEntradas(vista);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==vista.btnRegistrar){
+            model=ProcesosCliente.LeerDatos(vista);
+            crud=new CRUD_Cliente();
+            crud.InsertarCLiente(model);
+            AcualizarForm();
             
         }
+        
+        
+        if(e.getSource()==vista.btnConsultar){
+            int codigo=Mensajes.M2("Ingrese el Codigo de la Mascota");
+            crud=new CRUD_Cliente();
+            model=crud.RecuperarDatosdeServicio(codigo);
+            if(model==null){
+                Mensajes.M1("El ID CLIENTE : "+codigo+" no existe en la DB");
+            }else{
+                vista.txtNombres.setText(model.getNombres());
+                vista.txtApellidos.setText(model.getApellidos());
+                vista.txtDireccion.setText(model.getDireccion());
+                SimpleDateFormat df= new SimpleDateFormat("yyyy-MM-dd");
+                Date fechatxt;
+                try {
+                    fechatxt=df.parse(model.getFechaNac()+"");
+                    vista.jdcFechaNacimiento.setDate(fechatxt);
+                } catch (ParseException ex) {
+                    Mensajes.M1("No se puedo obtener un dato "+ex);
+                }
+                vista.txtTelefono.setText(model.getTelefono());
+                vista.txtEmail.setText(model.getEmail());
+                vista.txtIdCliente.setText(model.getIdCliente()+"");
+                vista.txtNombres.requestFocus();
+            }
+        }
+        
+        
+        if(e.getSource()==vista.btnActualizar){
+           model=ProcesosCliente.LeerDatos(vista);
+           model.setIdCliente(Integer.parseInt(vista.txtIdCliente.getText()));
+           crud=new CRUD_Cliente();
+           crud.ActualizarCliente(model);
+           AcualizarForm();
+       }
     }
     
 }
--- src/DAO/CRUD_Cliente.java
+++ src/DAO/CRUD_Cliente.java
@@ -2,6 +2,7 @@ package DAO;
 
 import Formato.ManejadorTablas;
 import Formato.Mensajes;
+import java.text.SimpleDateFormat;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import modelo.Cliente;
@@ -31,6 +32,65 @@ public class CRUD_Cliente extends ConectarDB{
             con.close();
         } catch (Exception e) {
             Mensajes.M1("ERROR Al obtener la tabla de Cliente -->"+e);
+        }
+    }
+    public void InsertarCLiente(Cliente cli){
+         try {
+            SimpleDateFormat df =  new SimpleDateFormat("yyyy-MM-dd");
+            ps=con.prepareStatement("insert into railway.Cliente(nombre,apellido,direccion,fechaNacimiento,telefono,email)" +
+                                    " values(?,?,?,?,?,?);");
+            ps.setString(1, cli.getNombres());
+            ps.setString(2, cli.getApellidos());
+            ps.setString(3, cli.getDireccion());
+            ps.setString(4,df.format(cli.getFechaNac()));
+            ps.setString(5, cli.getTelefono());
+            ps.setString(6, cli.getEmail());
+            ps.executeUpdate();
+            Mensajes.M1("Datos del Cliente : "+cli.getNombres()+" "+cli.getApellidos()+" registrados Correcatamente");
+            con.close();
+        } catch (Exception e) {
+            Mensajes.M1("ERROR al guardar datos en la Cliente -->"+e);
+        }
+    }
+    public Cliente RecuperarDatosdeServicio(int idCliente){
+        Cliente cli=null;
+        try {
+            rs=st.executeQuery("select * from railway.Cliente c where c.idCliente="+idCliente+";");
+            if(rs.next()){
+                cli=new Cliente();
+                cli.setIdCliente(rs.getInt(1));
+                cli.setNombres(rs.getString(2));
+                cli.setApellidos(rs.getString(3));
+                cli.setDireccion(rs.getString(4));
+                cli.setFechaNac(rs.getDate(5));
+                cli.setTelefono(rs.getString(6));
+                cli.setEmail(rs.getString(7));
+                
+            }
+            con.close();
+        } catch (Exception e) {
+            Mensajes.M1("Error no se puede recuperar datos del Id Cliente = "+idCliente+"   "+e);
+        }
+        return cli;
+    }
+    
+    public void ActualizarCliente(Cliente cli){
+        try {
+            SimpleDateFormat df =  new SimpleDateFormat("yyyy-MM-dd");
+            ps=con.prepareStatement("update railway.Cliente p set p.nombre=?,p.apellido=?,p.direccion=?,p.fechaNacimiento=?,p.telefono=?,p.email=?" +
+                                    " where p.idCliente=?;");
+            ps.setString(1, cli.getNombres());
+            ps.setString(2, cli.getApellidos());
+            ps.setString(3, cli.getDireccion());
+            ps.setString(4,df.format(cli.getFechaNac()));
+            ps.setString(5, cli.getTelefono());
+            ps.setString(6, cli.getEmail());
+            ps.setInt(7, cli.getIdCliente());
+            ps.executeUpdate();
+            Mensajes.M1("Datos del Cliente : "+cli.getNombres()+" "+cli.getApellidos()+" se Actualizaron Correctamente");
+            con.close();
+        } catch (Exception e) {
+            Mensajes.M1("Error a la hora de Actualizar Datos del Cliente: "+cli.getNombres()+" "+cli.getApellidos()+"   \n"+e); 
         }
     }
 }
